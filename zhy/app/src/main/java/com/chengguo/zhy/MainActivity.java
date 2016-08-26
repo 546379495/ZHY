de21867be92fce50d5347fad91513870d7c19235
nongfusanquanF <546379495@qq.com>
2016-08-26 08:36:02 +0800

    自定义ImageView 用于调用系统相机设置头像

--- zhy/app/src/main/java/com/chengguo/zhy/MainActivity.java
+++ zhy/app/src/main/java/com/chengguo/zhy/MainActivity.java
@@ -3,18 +3,26 @@ package com.chengguo.zhy;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
+import com.lidroid.xutils.ViewUtils;
 
 
 
 public class MainActivity extends FragmentActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ViewUtils.inject(this);
+
+
 //        initFragment();
     }
-//    public void initFragment(){
+
+
+    //    public void initFragment(){
 //        FragmentManager fm=getSupportFragmentManager();
 //        FragmentTransaction transaction= fm.beginTransaction();//开启事务
 //        transaction.replace(R.id.fl_content, new ConrentFragment());
