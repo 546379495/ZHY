1f2c09c029dfbd617a02f229e2d4f8665a7c9009
nongfusanquanF <546379495@qq.com>
2016-09-01 16:12:41 +0800

    “我的”页面布局

--- zhy/app/src/main/java/com/chengguo/zhy/base/impl/WoDePager.java
+++ zhy/app/src/main/java/com/chengguo/zhy/base/impl/WoDePager.java
@@ -2,10 +2,8 @@ package com.chengguo.zhy.base.impl;
 
 
 import android.app.Activity;
-import android.graphics.Color;
-import android.view.Gravity;
-import android.widget.TextView;
-
+import android.view.View;
+import com.chengguo.zhy.R;
 import com.chengguo.zhy.base.BasePager;
 
 /**
@@ -23,12 +21,10 @@ public class WoDePager extends BasePager {
 	public void initData() {
 
 		tvTitle.setText("我的");
-		TextView text=new TextView(mActivity);
-		text.setText("我的");
-		text.setTextSize(25);
-		text.setTextColor(Color.RED);
-		text.setGravity(Gravity.CENTER);
-		flContent.addView(text);
+		View view=View.inflate(mActivity, R.layout.wode_pager,null);
+
+
+		flContent.addView(view);
 		
 	}
 	
